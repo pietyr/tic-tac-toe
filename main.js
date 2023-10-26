c5d37aeccf6f5d64bc3a274ec60999147718930f
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2023-10-26 08:48:58 +0200

    feat: add 2 radio buttons

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 import './scss/main.scss';
+
+const buttonX = document.querySelector('#radio-x');
+const buttonO = document.querySelector('#radio-o');
+
+buttonX.addEventListener('click', (e) => {
+  console.log(`buttonX clicked`);
+  console.log(`checked = ${buttonX.checked}`);
+  console.log(`buttonO checked = ${buttonO.checked}`);
+});
+
+buttonO.addEventListener('click', (e) => {
+  console.log(`buttonO clicked`);
+  console.log(`checked = ${buttonO.checked}`);
+  console.log(`buttonX checked = ${buttonX.checked}`);
+});
