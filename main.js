c8591d92ba707d721123885f629704d89bd3070c
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2023-10-30 16:28:25 +0100

    feat: add changing x/o icon color on animation

--- main.js
+++ main.js
@@ -5,11 +5,24 @@ let playerMark = 'o';
 const buttonX = document.querySelector('#radio-x');
 const buttonO = document.querySelector('#radio-o');
 const radioSelector = document.querySelector('.game-settings__radio-selector');
+const xIcon = document.querySelector(
+  '.game-settings__radio-label-x > .game-settings__radio-image',
+);
+const oIcon = document.querySelector(
+  '.game-settings__radio-label-o > .game-settings__radio-image',
+);
 
 buttonX.addEventListener('click', () => {
   if (playerMark === 'o') {
     // radioSelector.style.animationDirection = 'reverse';
     radioSelector.style.animationPlayState = 'running';
+    setTimeout(() => {
+      xIcon.setAttribute('src', './assets/icon-x--dark.svg');
+    }, 100);
+
+    setTimeout(() => {
+      oIcon.setAttribute('src', './assets/icon-o--light.svg');
+    }, 300);
   }
   playerMark = 'x';
 });
@@ -18,6 +31,13 @@ buttonO.addEventListener('click', () => {
   if (playerMark === 'x') {
     // radioSelector.style.animationDirection = 'normal';
     radioSelector.style.animationPlayState = 'running';
+    setTimeout(() => {
+      oIcon.setAttribute('src', './assets/icon-o--dark.svg');
+    }, 100);
+
+    setTimeout(() => {
+      xIcon.setAttribute('src', './assets/icon-x--light.svg');
+    }, 300);
   }
   playerMark = 'o';
 });
@@ -25,3 +45,5 @@ buttonO.addEventListener('click', () => {
 radioSelector.addEventListener('animationiteration', () => {
   radioSelector.style.animationPlayState = 'paused';
 });
+
+radioSelector.addEventListener('animationstart', () => {});
