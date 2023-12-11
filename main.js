4bf7db3301f4ee905b6dc1212a0961f69b22dad2
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2023-12-11 17:02:50 +0100

    Revert "refactor: move code from main.js to separate files"
    This reverts commit a7eaa92cfbc2d1f0bc1c9551e819e750d6334e60.

--- main.js
+++ main.js
@@ -1,7 +1,70 @@
 import './scss/main.scss';
-import loadSettings from './js/new-game';
 
-loadSettings();
+let playerMark = 'o';
+
+const buttonX = document.querySelector('#radio-x');
+const buttonO = document.querySelector('#radio-o');
+const radioSelector = document.querySelector('.game-settings__radio-selector');
+const xIcon = document.querySelector(
+  '.game-settings__radio-label-x > .game-settings__radio-image',
+);
+const oIcon = document.querySelector(
+  '.game-settings__radio-label-o > .game-settings__radio-image',
+);
+const newGameCPUButton = document.querySelector(
+  '.game-settings__button--yellow',
+);
+const newGamePlayerButton = document.querySelector(
+  '.game-settings__button--blue',
+);
+const gameSettingsBoard = document.querySelector('.game-settings');
+const gameBoard = document.querySelector('.game');
+
+newGameCPUButton.addEventListener('click', () => {
+  gameSettingsBoard.style.display = 'none';
+  gameBoard.style.display = 'grid';
+});
+
+newGamePlayerButton.addEventListener('click', () => {
+  gameSettingsBoard.style.display = 'none';
+  gameBoard.style.display = 'grid';
+});
+
+buttonX.addEventListener('click', () => {
+  if (playerMark === 'o') {
+    // radioSelector.style.animationDirection = 'reverse';
+    radioSelector.style.animationPlayState = 'running';
+    setTimeout(() => {
+      xIcon.setAttribute('src', './assets/icon-x--dark.svg');
+    }, 100);
+
+    setTimeout(() => {
+      oIcon.setAttribute('src', './assets/icon-o--light.svg');
+    }, 300);
+  }
+  playerMark = 'x';
+});
+
+buttonO.addEventListener('click', () => {
+  if (playerMark === 'x') {
+    // radioSelector.style.animationDirection = 'normal';
+    radioSelector.style.animationPlayState = 'running';
+    setTimeout(() => {
+      oIcon.setAttribute('src', './assets/icon-o--dark.svg');
+    }, 100);
+
+    setTimeout(() => {
+      xIcon.setAttribute('src', './assets/icon-x--light.svg');
+    }, 300);
+  }
+  playerMark = 'o';
+});
+
+radioSelector.addEventListener('animationiteration', () => {
+  radioSelector.style.animationPlayState = 'paused';
+});
+
+radioSelector.addEventListener('animationstart', () => {});
 
 // GAME
 
